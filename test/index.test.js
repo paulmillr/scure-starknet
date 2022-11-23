bb393aea5fb75020db2f30deaa91876cbe1c7e1f
Paul Miller <paul@paulmillr.com>
2022-11-23 17:06:05 +0000

    Move tests to mjs

--- test/index.test.js
+++
@@ -1,6 +0,0 @@
-import { should } from 'micro-should';
-
-import './basic.test.js';
-import './starknet.test.js';
-
-should.run();
---
+++ test/index.test.mjs
@@ -0,0 +1,6 @@
+import { should } from 'micro-should';
+
+import './basic.test.mjs';
+import './starknet.test.mjs';
+
+should.run();
